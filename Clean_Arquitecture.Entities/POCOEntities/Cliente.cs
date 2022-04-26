5269ddbb66176af529061a6a42c6f96cde723f71
Abraham Hernandez <ahernandez@globalrisk.pe>
2022-04-26 16:25:59 -0500

    Creating project

---
+++ Clean_Arquitecture.Entities/POCOEntities/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Arquitecture.Entities.POCOEntities
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Clean_Arquitecture.Entities/POCOEntities/Orden.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Arquitecture.Entities.POCOEntities
+{
+    public class Orden
+    {
+        public int Id { get; set; }
+        public string ClienteId { get; set; }
+    }
+}
