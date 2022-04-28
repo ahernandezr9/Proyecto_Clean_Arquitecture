811aa796690c381d60efcc90362ddae03fe68539
Abraham Hernandez <ahernandez@globalrisk.pe>
2022-04-28 15:18:38 -0500

    //Branchita

--- Clean_Arquitecture.Entities/Specifications/Specification.cs
+++ Clean_Arquitecture.Entities/Specifications/Specification.cs
@@ -14,6 +14,7 @@ namespace Clean_Arquitecture.Entities.Specifications
         {
             Func<T, bool> ExpressionDelegate = Expression.Compile();
             return ExpressionDelegate(entity);
+        //Branchita
         }
     }
 }
