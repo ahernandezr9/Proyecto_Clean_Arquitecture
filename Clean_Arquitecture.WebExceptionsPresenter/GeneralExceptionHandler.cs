f9b56fafb39178477abe03f995d09422189801e8
Abraham Hernandez <ahernandez@globalrisk.pe>
2022-04-29 18:14:03 -0500

    Creating Presenters

---
+++ Clean_Arquitecture.WebExceptionsPresenter/GeneralExceptionHandler.cs
@@ -0,0 +1,20 @@
+﻿using Clean_Arquitecture.Entities.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Arquitecture.WebExceptionsPresenter
+{
+    public class GeneralExceptionHandler : ExceptionHandlerBase, IExceptionHandler
+    {
+        public Task Handle(ExceptionContext context)
+        {
+            var Exception = context.Exception as GeneralException;
+            return SetResult(context, StatusCodes.Status500InternalServerError, Exception.Message, Exception.Detail);
+        }
+    }
+}
