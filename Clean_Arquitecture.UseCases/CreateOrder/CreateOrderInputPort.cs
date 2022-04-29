1166a5e53b5677bde90f36c7fa5ede822693a2ef
Abraham Hernandez <ahernandez@globalrisk.pe>
2022-04-29 01:19:45 -0500

    Creating UseCases

---
+++ Clean_Arquitecture.UseCases/CreateOrder/CreateOrderInputPort.cs
@@ -0,0 +1,14 @@
+﻿using Clean_Arquitecture.UseCasesDTOs.CreateOrder;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Arquitecture.UseCases.CreateOrder
+{
+    public class CreateOrderInputPort : CreateOrderParams, IRequest<int>
+    {
+    }
+}
